d034fc137945813042bbbda54519d7110e09b03a
Artem Zhelonkin <tyo3436gmail.com>
2021-01-21 21:37:21 +0300

    Добавлены условия задачи в класс DiningPhilosopher

--- philosophers/dining.py
+++ philosophers/dining.py
@@ -12,32 +12,46 @@ class DiningPhilosopher(threading.Thread):
     def __init__(self, philosopher):
         threading.Thread.__init__(self)
         self.philosopher = philosopher      # philosopher id
-        self.left_fork = philosopher
-        self.right_fork = (philosopher+1) % 5
+        self.hold_forks = 0
 
+
+    # всегда первой берем левую вилку
     # взять левую вилку
     def pickLeftFork(self):
-        # TODO: implement
-        pass
+        left_fork_number = self.philosopher
+        self.hold_forks = 1
+        print(left_fork_number)
 
+    # если мы пытаемся взять правую вилку, то левую мы уже держим
     # взять правую вилку
     def pickRightFork(self):
-        pass
+        right_fork_number = (self.philosopher + 1) % 5
+        self.hold_forks = 2
+        print(right_fork_number)
 
+    # если едим, то держим обе вилки
     # есть
     def eat(self):
-        pass
+        if self.hold_forks == 2:
+            print("OK")
+        else:
+            print("I can't! Give me forks!")
 
+
+    # всегда опускаем первой левую вилку
     # положить левую вилку
     def putLeftFork(self):
-        pass
+        self.hold_forks = 1
 
+    # второй опускаем правую вилку
     # положить правую вилку
     def putRightFork(self):
-        pass
+        self.hold_forks = 0
 
 
 philosopher_1 = DiningPhilosopher(0)
 philosopher_2 = DiningPhilosopher(1)
 
-print(philosopher_1.philosopher, philosopher_2.left_fork, philosopher_2.right_fork)
+print(philosopher_1.philosopher)
+philosopher_1.pickLeftFork()
+print(philosopher_1.hold_forks)
